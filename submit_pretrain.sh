615571651a8ee10103d2930cd42401b1910743bf
valerie-engel <valerie.engelmayer@gmail.com>
2026-01-15 13:53:15 +0100

    build container

--- submit_pretrain.sh
+++ submit_pretrain.sh
@@ -9,24 +9,4 @@
 #SBATCH -e logs/slurm%j.err
 #SBATCH -J pretrain       # job name
 
-# singularity exec --nv container/container.sif bash -c "
-# source /opt/conda/etc/profile.d/conda.sh
-# conda activate hauner
-# python - << 'EOF'
-# import torch_sparse
-# from torch_geometric.loader import NeighborSampler
-# print('NeighborSampler OK')
-# EOF
-# "
-
-
-# singularity exec --nv container/container2.sif bash -c "
-# source /opt/conda/etc/profile.d/conda.sh
-# conda activate hauner
-# export LD_LIBRARY_PATH=\$CONDA_PREFIX/lib/python3.10/site-packages/torch/lib:\$CONDA_PREFIX/lib:\$LD_LIBRARY_PATH
-# python pretrain.py
-# "
-
-#/opt/conda/bin/conda run -n hauner 
-
 singularity exec --nv container/container.sif python pretrain.py
